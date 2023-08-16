66d78962fad48d86128ba27653ff524e46eb9610
naseem173 <md.naseem0978@gmail.com>
2023-08-16 12:51:11 +0530

    upper to lower

---
+++ challenge-Strings/upper2lowerAndViceVersa.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include<string>
+#include<cctype>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (isupper(s[i]))
+        {
+            s[i] = tolower(s[i]);
+        }
+
+        else if (islower(s[i]))
+        {
+            s[i] = toupper(s[i]);
+        }
+    }
+    cout << s << endl;
+
+    return 0;
+}
