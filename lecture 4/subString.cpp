d4acfa9ee3b00576637f0784d3b11b4dcfa34651
naseem173 <md.naseem0978@gmail.com>
2023-08-21 20:15:51 +0530

    Sub String problem of abc

---
+++ lecture 4/subString.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+void subsequence(char*in, int i, char*out, int j){
+    //base case
+    if(in[i]=='\0'){
+        out[j]='\0';
+        cout<< out<<endl;
+        return;
+    }
+
+
+    //recursive case
+
+    subsequence(in,i+1,out,j);
+
+    out[j]=in[i];
+    subsequence(in,i+1,out,j+1);
+
+}
+
+int main(){
+
+    char a[]="abc";
+    char out[100];
+    subsequence(a,0,out,0);
+    return 0;
+}
