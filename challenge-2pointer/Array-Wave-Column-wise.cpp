a111eb4222a60b7a859b16a6b48a563db0a6d242
naseem173 <md.naseem0978@gmail.com>
2023-09-03 01:25:42 +0530

    print elements as Array wave column wise

---
+++ challenge-2pointer/Array-Wave-Column-wise.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    int m, n;
+    cin >> m >> n;
+    vector<vector<int> >a (m,vector<int>(n));
+
+    for (int i = 0; i < m; i++) {
+        
+        for (int j = 0; j < n; j++)
+        {
+           cin >> a[i][j];
+        }
+    }
+     int top=0, bottom=m-1,left=0,right=n-1;
+    bool isMovingDown = true;
+    //bool isMovingRight = false;
+    bool isMovingUp = false;
+    //bool isMovingLeft = false;
+
+     while (left <= right) {
+
+         if (isMovingDown) {
+            for (int i = top; i <= bottom; i++) {
+                cout << a[i][left] << ", ";
+            }
+            left++;
+            isMovingDown = false;
+            isMovingUp = true;
+            
+            }
+        
+        else if (isMovingUp) {
+            for (int i = bottom; i >= top; i--) {
+                cout << a[i][left] << ", ";
+            }
+            left++;
+            isMovingUp = false;
+            isMovingDown = true;
+            
+            }
+        }
+
+//11, 21, 31, 41, 42,
+// 32, 22, 12, 13, 23,
+// 33, 43, 44, 34, 24, 14, END
+     
+     cout << "END"<<endl;
+
+     return 0;
+ 
+    }
